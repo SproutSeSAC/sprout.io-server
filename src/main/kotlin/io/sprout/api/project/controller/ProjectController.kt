b7914c59babe48aaea0450eb4e72ab9c83c06613
sonsanghee <sonsanghee3@gmail.com>
2024-09-19 00:57:23 +0900

    change7

--- src/main/kotlin/io/sprout/api/project/controller/ProjectController.kt
+++ src/main/kotlin/io/sprout/api/project/controller/ProjectController.kt
@@ -1,13 +1,12 @@
 package io.sprout.api.project.controller
 
+import io.sprout.api.project.model.dto.ProjectFilterRequest
 import io.sprout.api.project.model.dto.ProjectRecruitmentRequestDto
+import io.sprout.api.project.model.dto.ProjectResponseDto
 import io.sprout.api.project.service.ProjectService
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/project")
@@ -24,4 +23,20 @@ class ProjectController(
         val result = projectService.postProject(dto)
         return ResponseEntity.ok(result)
     }
+
+    @Operation(
+        summary = "프로젝트 조회 필터 API", // 간단한 설명
+        description = "프로젝트 조회 필터에 관한 API 입니다.", // 상세 설명
+    )
+    @GetMapping
+    fun filterProjects(
+        @RequestParam(required = false) techStack: List<Long>?,
+        @RequestParam(required = false) position: String?,
+        @RequestParam(required = false) meetingType: String?
+    ): ResponseEntity<List<ProjectResponseDto>> {
+        val filterRequest = ProjectFilterRequest(techStack, position, meetingType)
+        val filteredProjects = projectService.getFilteredProjects(filterRequest)
+        return ResponseEntity.ok(filteredProjects)
+    }
+
 }
