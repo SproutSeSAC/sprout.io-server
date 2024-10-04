1bbdd8b7bea05955686c4dd9ac1bfb736017e76d
unknown <sonsanghee3@gmail.com>
2024-10-04 15:53:43 +0900

    change: localdatetime 포멧

--- src/main/kotlin/io/sprout/api/project/model/dto/ProjectCommentResponseDto.kt
+++ src/main/kotlin/io/sprout/api/project/model/dto/ProjectCommentResponseDto.kt
@@ -1,11 +1,13 @@
 package io.sprout.api.project.model.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.querydsl.core.annotations.QueryProjection
 import java.time.LocalDateTime
 
 data class ProjectCommentResponseDto @QueryProjection constructor(
     val id : Long,
     val content : String,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     val createdAt : LocalDateTime,
     val writer : String,
     val projectId : Long,
