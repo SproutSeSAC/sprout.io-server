d9a94f68bc549d5f866a384c196da5e1ea800678
unknown <sonsanghee3@gmail.com>
2024-10-25 17:43:48 +0900

    add: calender api

---
+++ src/main/kotlin/io/sprout/api/user/model/dto/ManagerEmailResponseDto.kt
@@ -0,0 +1,27 @@
+package io.sprout.api.user.model.dto
+
+import io.sprout.api.user.model.entities.RoleType
+import io.sprout.api.user.model.entities.UserEntity
+
+data class ManagerEmailResponseDto(
+    val id: Long,
+    val email: String,
+    val nickname: String,
+    val roleType: RoleType
+) {
+
+
+    companion object {
+        fun toDto(userEntity: UserEntity): ManagerEmailResponseDto {
+            return ManagerEmailResponseDto(
+                id = userEntity.id,
+                email = userEntity.email, // calendarId가 실제로 id와 다른 값이면 수정 필요
+                nickname = userEntity.nickname,
+                roleType = userEntity.role,
+            )
+        }
+
+
+    }
+}
+
