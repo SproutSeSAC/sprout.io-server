cb01635fb00629d41cbf04f78273cabc1b3415a0
woong <woong7361@gmail.com>
2024-11-19 17:04:07 +0900

    fix: 맛집 검색 수정

--- src/main/kotlin/io/sprout/api/store/controller/StoreController.kt
+++ src/main/kotlin/io/sprout/api/store/controller/StoreController.kt
@@ -1,6 +1,7 @@
 package io.sprout.api.store.controller
 
 import io.sprout.api.store.model.dto.StoreDto
+import io.sprout.api.store.model.dto.StoreProjectionDto
 import io.sprout.api.store.model.entities.FoodType
 import io.sprout.api.store.service.StoreService
 import io.swagger.v3.oas.annotations.Operation
@@ -22,48 +23,11 @@ class StoreController(
     @GetMapping("/list")
     @Operation(summary = "맛집 리스트 조회", description = "맛집 리스트 조회, 필터 체크되는 경우만 true, 아닐 경우 false")
     fun getStoreList(
-        @RequestParam campusId: Long,
-        @RequestParam(defaultValue = "false") isZeropay: Boolean,
-        @RequestParam(defaultValue = "false") underPrice: Boolean,
-        @RequestParam(defaultValue = "false") overFivePerson: Boolean,
-        @RequestParam(defaultValue = "false") walkTimeWithinFiveMinutes: Boolean,
-        @RequestParam foodTypeList: MutableSet<FoodType> = LinkedHashSet(),
-        @RequestParam(defaultValue = "1") page: Int,
-        @RequestParam(defaultValue = "20") size: Int
+        @ModelAttribute filterRequest: StoreDto.StoreListRequest
     ): ResponseEntity<Map<String, Any?>> {
+        val storeList: List<StoreProjectionDto.StoreInfoDto> = storeService.getStoreList(filterRequest)
 
-        val pageIndex = page - 1
-        val filterRequest = StoreDto.StoreListRequest(
-            campusId,
-            isZeropay,
-            underPrice,
-            overFivePerson,
-            walkTimeWithinFiveMinutes,
-            foodTypeList,
-            pageIndex,
-            size
-        )
-
-        val (storeList, totalCount) = storeService.getStoreList(filterRequest)
-
-        val totalPages = if (totalCount % size == 0L) {
-            totalCount / size
-        } else {
-            totalCount / size + 1
-        }
-
-        val nextPage = if (page < totalPages) page + 1 else null
-
-        val responseBody = mapOf(
-            "storeList" to storeList,
-            "totalCount" to totalCount,
-            "currentPage" to page,  // 프론트에서 보낸 페이지 번호 반환 (1부터 시작)
-            "pageSize" to size,
-            "totalPages" to totalPages,  // 총 페이지 수
-            "nextPage" to nextPage  // 다음 페이지 (마지막 페이지일 경우 null)
-        )
-
-        return ResponseEntity.ok(responseBody)
+        return ResponseEntity.ok(mapOf("stores" to storeList))
     }
 
     @GetMapping("/filterCount")
--- src/main/kotlin/io/sprout/api/store/repository/StoreRepositoryCustomImpl.kt
+++ src/main/kotlin/io/sprout/api/store/repository/StoreRepositoryCustomImpl.kt
@@ -2,56 +2,34 @@ package io.sprout.api.store.repository
 
 import com.querydsl.core.group.GroupBy.groupBy
 import com.querydsl.core.group.GroupBy.list
+import com.querydsl.core.types.ExpressionUtils
+import com.querydsl.core.types.ExpressionUtils.count
 import com.querydsl.core.types.Projections
 import com.querydsl.core.types.dsl.BooleanExpression
+import com.querydsl.jpa.JPAExpressions
 import com.querydsl.jpa.impl.JPAQueryFactory
 import io.sprout.api.store.model.dto.StoreDto
 import io.sprout.api.store.model.dto.StoreProjectionDto
-import io.sprout.api.store.model.entities.FoodType
-import io.sprout.api.store.model.entities.QStoreEntity
-import io.sprout.api.store.model.entities.QStoreImageEntity
-import io.sprout.api.store.model.entities.QStoreMenuEntity
+import io.sprout.api.store.model.entities.*
+import org.springframework.util.StringUtils
 
 class StoreRepositoryCustomImpl(
     private val jpaQueryFactory: JPAQueryFactory
 ) : StoreRepositoryCustom {
 
-    override fun findStoreList(request: StoreDto.StoreListRequest): Pair<List<StoreProjectionDto.StoreInfoDto>, Long> {
+    override fun findStoreList(request: StoreDto.StoreListRequest, userId: Long): List<StoreProjectionDto.StoreInfoDto> {
         val store = QStoreEntity.storeEntity
         val storeImage = QStoreImageEntity.storeImageEntity
         val storeMenu = QStoreMenuEntity.storeMenuEntity
-
-        val totalCount = jpaQueryFactory
-            .select(store.id)
-            .from(store)
-            .leftJoin(storeMenu).on(store.id.eq(storeMenu.store.id))
-            .leftJoin(storeImage).on(store.id.eq(storeImage.store.id))
-            .where(
-                searchInCampus(request.campusId),
-                searchInZeropay(request.isZeropay),
-                searchInWalkTime(request.walkTimeWithinFiveMinutes),
-                searchInFoodType(request.foodTypeList),
-                searchInPerson(request.overFivePerson),
-                searchInUnderPrice(request.underPrice),
-            )
-            .groupBy(store.id)
-            .fetch()
-
-        val storeIdsForPagination= jpaQueryFactory
-            .select(store.id)
-            .from(store)
-            .orderBy(store.id.asc())
-            .limit(request.size.toLong())
-            .offset((request.page * request.size).toLong())
-            .fetch()
+        val storeScrap = QScrapedStoreEntity.scrapedStoreEntity
 
         val result = jpaQueryFactory
             .selectFrom(store)
-            .leftJoin(storeMenu).on(store.id.eq(storeMenu.store.id))
-            .leftJoin(storeImage).on(store.id.eq(storeImage.store.id))
+            .distinct()
+            .leftJoin(storeMenu).on(store.id.eq(storeMenu.store.id)).fetchJoin()
             .where(
-                store.id.`in`(storeIdsForPagination),
                 searchInCampus(request.campusId),
+                containKeywordsInName(request.keyword),
                 searchInZeropay(request.isZeropay),
                 searchInWalkTime(request.walkTimeWithinFiveMinutes),
                 searchInFoodType(request.foodTypeList),
@@ -59,7 +37,8 @@ class StoreRepositoryCustomImpl(
                 searchInUnderPrice(request.underPrice),
             )
             .orderBy(store.id.asc())
-            .distinct()
+            .limit(request.size.toLong())
+            .offset((request.pageIndex * request.size).toLong())
             .transform(
                 groupBy(store.id).list(
                     Projections.constructor(
@@ -68,7 +47,6 @@ class StoreRepositoryCustomImpl(
                         store.name,
                         store.foodType,
                         store.campus.name,
-                        store.mapSchemaUrl,
                         store.address,
                         store.contact,
                         store.workingDay,
@@ -77,26 +55,66 @@ class StoreRepositoryCustomImpl(
                         store.isZeropay,
                         store.walkTime,
                         store.isOverPerson,
-                        list(
-                            Projections.constructor(
-                                StoreProjectionDto.StoreImageDto::class.java,
-                                storeImage.id,
-                                storeImage.path
-                            )
+                        store.longitude,
+                        store.latitude,
+                        ExpressionUtils.`as`(
+                            JPAExpressions.select(count(storeScrap.id))
+                                .from(storeScrap)
+                                .where(
+                                    storeScrap.store.id.eq(store.id)
+                                ), "scrapCount"
                         ),
-                        list(
-                            Projections.constructor(
-                                StoreProjectionDto.StoreMenuDto::class.java,
-                                storeMenu.id,
-                                storeMenu.name,
-                                storeMenu.price
-                            )
+                        ExpressionUtils.`as`(
+                            JPAExpressions.select(storeScrap.id)
+                                .from(storeScrap)
+                                .where(
+                                    storeScrap.store.id.eq(store.id)
+                                        .and(storeScrap.user.id.eq(userId))
+                                )
+                                .exists(),
+                            "isScrap"
                         )
                     )
                 )
             )
 
-        return Pair(result, totalCount.size.toLong())
+        val searchedStoreIds: List<Long> = result.map { it.id }.toList()
+
+        val images: List<StoreImageEntity> = jpaQueryFactory
+            .select(storeImage)
+            .from(storeImage)
+            .distinct()
+            .where(
+                storeImage.store.id.`in`(searchedStoreIds)
+            )
+            .orderBy(storeImage.store.id.asc())
+            .fetch()
+        result.forEach {res -> images.forEach {img ->
+                if (res.id == img.store.id) {
+                    res.storeImageList.add(
+                        StoreProjectionDto.StoreImageDto(img.id, img.path))
+                }
+            }
+        }
+
+        val menus: List<StoreMenuEntity> = jpaQueryFactory
+            .select(storeMenu)
+            .from(storeMenu)
+            .distinct()
+            .where(
+                storeMenu.store.id.`in`(searchedStoreIds)
+            )
+            .orderBy(storeMenu.store.id.asc())
+            .fetch()
+        result.forEach {res -> menus.forEach {menu ->
+                if (res.id == menu.store.id) {
+                    res.storeMenuList.add(
+                        StoreProjectionDto.StoreMenuDto(menu.id, menu.name, menu.price, menu.imageUrl))
+                }
+            }
+        }
+
+        return result
     }
 
     override fun findStoreFilterList(campusId: Long): List<StoreProjectionDto.StoreFilterDto> {
@@ -136,6 +154,14 @@ class StoreRepositoryCustomImpl(
     // Dynamic predicates
     private fun searchInCampus(campusId: Long): BooleanExpression? {
         return QStoreEntity.storeEntity.campus.id.eq(campusId)
+    }
+
+    private fun containKeywordsInName(keyword: String): BooleanExpression? {
+        return if (StringUtils.hasText(keyword)) {
+            QStoreEntity.storeEntity.name.contains(keyword)
+        } else {
+            null
+        }
     }
 
     private fun searchInZeropay(isZeropay: Boolean): BooleanExpression? {
