75d5c4a76f52775821126138f8ddab18616e9c7b
woong <woong7361@gmail.com>
2024-11-20 18:00:42 +0900

    fix: 한끼팟 삭제 수정

---
+++ src/main/kotlin/io/sprout/api/mealPost/repository/MealPostParticipationRepository.kt
@@ -0,0 +1,16 @@
+package io.sprout.api.mealPost.repository
+
+import io.sprout.api.mealPost.model.entities.MealPostParticipationEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
+
+@Repository
+interface MealPostParticipationRepository: JpaRepository<MealPostParticipationEntity, Long> {
+
+    @Query("SELECT count(mpp.id) > 0 " +
+            "FROM MealPostParticipationEntity mpp " +
+            "WHERE mpp.mealPost.id = :mealPostId AND mpp.user.id = :userId AND mpp.ordinalNumber = 1")
+    fun isOwner(mealPostId: Long, userId: Long): Boolean
+
+}
