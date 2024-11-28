64a016250d045c0571211a92c36eaaa117104a19
woong <woong7361@gmail.com>
2024-11-28 15:03:37 +0900

    fix: 공지사항 swagger 추가

--- src/main/kotlin/io/sprout/api/notice/controller/NoticeController.kt
+++ src/main/kotlin/io/sprout/api/notice/controller/NoticeController.kt
@@ -3,6 +3,7 @@ package io.sprout.api.notice.controller
 import io.sprout.api.notice.model.dto.*
 import io.sprout.api.notice.model.entities.ParticipantStatus
 import io.sprout.api.notice.service.NoticeService
+import io.swagger.v3.oas.annotations.Operation
 import jakarta.validation.Valid
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
@@ -23,6 +24,7 @@ class NoticeController(
      * @return noticeId
      */
     @PostMapping
+    @Operation(summary = "공지사항 등록", description = "공지사항을 등록합니다.")
     fun createNotice(@RequestBody @Valid noticeRequest: NoticeRequestDto): ResponseEntity<Map<String, Long>> {
         val noticeId = noticeService.createNotice(noticeRequest)
 
@@ -36,6 +38,7 @@ class NoticeController(
      * @param noticeId 수정할 공지사항 ID
      */
     @PutMapping("/{noticeId}")
+    @Operation(summary = "공지사항 수정", description = "PUT 방식으로 공지사항 등록과 동일하게 보내면 됩니다.")
     fun updateNotice(
         @PathVariable noticeId: Long, @RequestBody noticeRequest: NoticeRequestDto): ResponseEntity<Map<String, Long>> {
         noticeService.updateNotice(noticeId, noticeRequest)
@@ -49,6 +52,7 @@ class NoticeController(
      * @param noticeId 조회할 공지사항 ID
      */
     @GetMapping("/{noticeId}")
+    @Operation(summary = "공지사항 조회", description = "공지사항을 조회합니다. 댓글은 포함되어있지 않습니다.")
     fun getNoticeById(@PathVariable noticeId: Long): ResponseEntity<NoticeDetailResponseDto> {
         val response = noticeService.getNoticeById(noticeId)
 
@@ -63,6 +67,7 @@ class NoticeController(
      * @param searchRequest 공지사항 검색 파라미터
      */
     @GetMapping
+    @Operation(summary = "공지사항 검색", description = "공지사항을 검색합니다. keyword는 제목과 내용중 포함되는 부분을 찾습니다.")
     fun getNotices(
         @ModelAttribute searchRequest: NoticeSearchRequestDto
     ): ResponseEntity<Map<String, Any?>> {
@@ -78,6 +83,7 @@ class NoticeController(
      * @param pageable 페이지네이션 요청 파라미터
      */
     @GetMapping("/{noticeId}/comments")
+    @Operation(summary = "공지사항 댓글 조회", description = "공지사항의 댓글을 조회합니다. 무한스크롤 형태로 되어있습니다.")
     fun getNoticeComments(
         @PathVariable noticeId: Long,
         pageable: Pageable
@@ -94,6 +100,7 @@ class NoticeController(
      * @param commentRequest 댓글 생성 요청 파라미터
      */
     @PostMapping("/{noticeId}/comments")
+    @Operation(summary = "공지사항 댓글 작성", description = "댓글을 작성합니다.")
     fun createNoticeComment(
         @PathVariable noticeId: Long,
         @RequestBody commentRequest: NoticeCommentRequestDto
@@ -109,6 +116,7 @@ class NoticeController(
      * @param commentId 삭제할 댓글 ID
      */
     @DeleteMapping("/comments/{commentId}")
+    @Operation(summary = "공지사항 댓글 삭제", description = "댓글을 삭제합니다.")
     fun deleteNoticeComment(
         @PathVariable commentId: Long
     ): ResponseEntity<Any> {
@@ -124,6 +132,7 @@ class NoticeController(
      * @param noticeId 삭제할 공지사항 ID
      */
     @DeleteMapping("/{noticeId}")
+    @Operation(summary = "공지사항을 삭제합니다.", description = "연관된 모든 것을 삭제합니다.")
     fun deleteNotice(@PathVariable noticeId: Long) {
         noticeService.deleteNotice(noticeId)
     }
@@ -133,6 +142,7 @@ class NoticeController(
      *
      * @param sessionId 공지사항 강의 세션 ID
      */
+    @Operation(summary = "공지사항 세션에 참가 신청합니다.", description = "참가 대기 상태로 신청하게 됩니다.")
     @PostMapping("/sessions/{sessionId}/application")
     fun applyForNoticeSession(@PathVariable sessionId: Long): ResponseEntity<Any> {
         noticeService.applyForNoticeSession(sessionId)
@@ -147,6 +157,7 @@ class NoticeController(
      * @param participantId 강의 참가 요청 ID
      */
     @PostMapping("/sessions/{sessionId}/accept/{participantId}")
+    @Operation(summary = "공지사항 세션 참가 수락", description = "참가 대기상태 또는 거절상태를 참가상태로 바꿉니다.")
     fun acceptParticipationRequest(
         @PathVariable sessionId: Long,
         @PathVariable participantId: Long
@@ -163,6 +174,7 @@ class NoticeController(
      * @param participantId 강의 참가 요청 ID
      */
     @PostMapping("/sessions/{sessionId}/reject/{participantId}")
+    @Operation(summary = "공지사항 세션 참가 거절", description = "대기상태 또는 참가상태를 거절상태로 바꿉니다.")
     fun rejectParticipationRequest(
         @PathVariable sessionId: Long,
         @PathVariable participantId: Long
@@ -179,6 +191,7 @@ class NoticeController(
      * @param participantId 강의 참가 요청 ID
      */
     @DeleteMapping("/sessions/{sessionId}/cancel/{participantId}")
+    @Operation(summary = "공지사항 세션 참가 취소", description = "공지사항 참가 상태를 삭제합니다. (DB 삭제)")
     fun cancelParticipantRequest(
         @PathVariable sessionId: Long,
         @PathVariable participantId: Long
@@ -192,6 +205,7 @@ class NoticeController(
      * 공지사항 세션 확인 (신청자, 참가자, ... 포함)
      */
     @GetMapping("/sessions/{sessionId}")
+    @Operation(summary = "공지사항 세션 참가자 확인", description = "공지사항 세션 참가자를 확인합니다.")
     fun getSessionDetail(
         @PathVariable sessionId: Long,
         @RequestParam(defaultValue = "1") page: Int,
