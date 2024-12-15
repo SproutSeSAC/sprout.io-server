0ea8aa8ff9a2fcafd75b94b2107f0acaeb1dbe3e
devbini <cong8685@naver.com>
2024-12-16 04:29:06 +0900

    feat: 레퍼런스 ID 읽는 API 추가

--- src/main/kotlin/io/sprout/api/post/controller/PostController.kt
+++ src/main/kotlin/io/sprout/api/post/controller/PostController.kt
@@ -1,13 +1,12 @@
 package io.sprout.api.post.controller
 
 import io.sprout.api.comment.dto.CommentResponseDto
-import io.sprout.api.comment.entity.CommentEntity
 import io.sprout.api.comment.service.CommentService
 import io.sprout.api.notice.model.dto.NoticeRequestDto
-import io.sprout.api.post.dto.PostResponseDto
 import io.sprout.api.post.service.PostService
 import io.sprout.api.project.model.dto.ProjectRecruitmentRequestDto
 import io.swagger.v3.oas.annotations.Operation
+import org.apache.naming.ResourceRef
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -18,6 +17,18 @@ class PostController(
     private val postService: PostService,
     private val commentService: CommentService
 ) {
+
+    @Operation(summary = "상세 글 ID 읽기")
+    @GetMapping("/gerorigin/{post_id}")
+    fun getRefInfo(@PathVariable post_id: Long): ResponseEntity<Any> {
+        return try {
+            val result = postService.getOrigin(post_id)
+            ResponseEntity.ok(result)
+        } catch (e: Exception) {
+            val errorResponse = ErrorResponse("ID 읽기 실패", e.message ?: "로그 확인")
+            ResponseEntity(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
 
     @Operation(summary = "공지사항 글 생성")
     @PostMapping("/notice")
