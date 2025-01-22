10e1a0c88784ad2b28e2009f0b1337818da9f468
devbini <cong8685@naver.com>
2025-01-23 05:58:40 +0900

    fix: 어노테이션, COnfig 최신버전화

--- src/main/kotlin/io/sprout/api/config/properties/CorsPropertiesConfig.kt
+++ src/main/kotlin/io/sprout/api/config/properties/CorsPropertiesConfig.kt
@@ -1,9 +1,7 @@
 package io.sprout.api.config.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
 
-@Configuration
 @ConfigurationProperties(prefix = "cors")
 data class CorsPropertiesConfig(
     var allowedOrigins: Array<String> = arrayOf()
