483151fb7147a4e0aa3f89665e0a409375bb70a9
woong <woong7361@gmail.com>
2025-02-02 10:29:11 +0900

    test: local용 로그인 token api 추가

--- src/main/kotlin/io/sprout/api/test/OriginTestController.kt
+++ src/main/kotlin/io/sprout/api/test/OriginTestController.kt
@@ -1,6 +1,8 @@
 package io.sprout.api.test
 
 import io.sprout.api.auth.token.domain.JwtToken
+import io.sprout.api.common.exeption.custom.CustomBadRequestException
+import io.sprout.api.user.repository.UserRepository
 import io.sprout.api.user.service.UserService
 import io.swagger.v3.oas.annotations.Operation
 import jakarta.servlet.http.Cookie
@@ -8,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,7 +18,8 @@ import org.springframework.web.bind.annotation.RestController
 class OriginTestController(
     private val jwtToken: JwtToken,
     private val testService: OriginTestService,
-    private val userService: UserService
+    private val userService: UserService,
+    private val userRepository: UserRepository
 ) {
     @Value("\${user.email}")
     lateinit var email: String
@@ -28,6 +32,46 @@ class OriginTestController(
     @GetMapping("/getAdminCookie")
     @Operation(summary = "Application 계정 단편 로그인")
     fun getTokens(response: HttpServletResponse): Map<String, String?> {
+        val user = userService.checkAndJoinUser(email, response)
+
+        val accessToken = user.refreshToken?.let { refreshToken ->
+            jwtToken.createAccessFromRefreshToken(refreshToken)
+        }
+
+        user.refreshToken?.let { refreshToken ->
+            val refreshCookie = Cookie("refresh_token", refreshToken).apply {
+                path = "/"
+                isHttpOnly = false
+                secure = false
+                setAttribute("SameSite", "None")
+            }
+            response.addCookie(refreshCookie)
+        }
+
+        accessToken?.let { token ->
+            val accessCookie = Cookie("access_token", token).apply {
+                path = "/"
+                isHttpOnly = false
+                secure = false
+                setAttribute("SameSite", "None")
+            }
+            response.addCookie(accessCookie)
+        }
+
+        return mapOf(
+            "refresh_token" to user.refreshToken,
+            "access_token" to accessToken
+        )
+    }
+
+    @GetMapping("/getUserCookie")
+    @Operation(summary = "Application 계정 단편 로그인")
+    fun getUserTokens(
+        @RequestParam email: String,
+        response: HttpServletResponse
+    ): Map<String, String?> {
+        userRepository.findByEmail(email) ?: throw CustomBadRequestException("not exist email")
+
         val user = userService.checkAndJoinUser(email, response)
 
         val accessToken = user.refreshToken?.let { refreshToken ->
