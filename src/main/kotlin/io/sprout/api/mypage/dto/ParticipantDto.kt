0002cde8996807b1c4192ff7a06459c0cd0058b4
devbini <cong8685@naver.com>
2025-02-21 03:36:42 +0900

    feat: 참여 특강 조회 시, 가까운 3개의 특강도 함께 반환토록 추가

--- src/main/kotlin/io/sprout/api/mypage/dto/ParticipantDto.kt
+++ src/main/kotlin/io/sprout/api/mypage/dto/ParticipantDto.kt
@@ -2,10 +2,18 @@ package io.sprout.api.mypage.dto
 
 import java.time.LocalDateTime
 
+data class nearParticipantDto(
+    val title: String,
+    val id: Long,
+    val startDateTime: LocalDateTime,
+    val endDateTime: LocalDateTime,
+)
+
 data class ParticipantDto (
     val title: String,
     val id: Long,
     val participantId: Long,
     val startDateTime: LocalDateTime,
     val endDateTime: LocalDateTime,
+    val nearParticipant: MutableList<nearParticipantDto>
 )
