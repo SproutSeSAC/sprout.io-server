14be05662a1a376602b43c9081a6f8e4d1387b7f
woong <woong7361@gmail.com>
2025-02-26 18:04:21 +0900

    hot fix: 매니저 이메일 반환 API 수정

--- src/main/kotlin/io/sprout/api/user/repository/UserRepositoryCustomImpl.kt
+++ src/main/kotlin/io/sprout/api/user/repository/UserRepositoryCustomImpl.kt
@@ -48,7 +48,10 @@ class UserRepositoryCustomImpl(
                 userCourseEntity.course.id.eq(courseId)
                     .and(
                         userEntity.role.eq(RoleType.CAMPUS_LEADER)
+                            .or(userEntity.role.eq(RoleType.OPERATION_MANAGER))
+                            .or(userEntity.role.eq(RoleType.EDU_MANAGER))
                             .or(userEntity.role.eq(RoleType.JOB_COORDINATOR))
+                            .or(userEntity.role.eq(RoleType.INSTRUCTOR))
                     )
             )
             .fetch()
