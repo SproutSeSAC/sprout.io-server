fcf0e3d81890978a8d32826d6e9e2fd757595482
woong <woong7361@gmail.com>
2025-04-29 11:46:30 +0900

    feat: 공지사항 세션 조회 API 추가

---
+++ src/main/kotlin/io/sprout/api/notice/model/dto/NoticeSessionResponseDto.kt
@@ -0,0 +1,45 @@
+package io.sprout.api.notice.model.dto
+
+import io.sprout.api.notice.model.entities.*
+import io.sprout.api.user.model.entities.RoleType
+import java.time.LocalDateTime
+
+data class NoticeSessionResponseDto(
+    var postId: Long,
+    val noticeId: Long,
+    val title: String,
+    val noticeType: NoticeType,
+    val status: NoticeStatus,
+    val createdAt: LocalDateTime,
+
+    var applicationStartDateTime: LocalDateTime? = null,
+    var applicationEndDateTime: LocalDateTime? = null,
+    var meetingPlace: String? = null,
+    var meetingType: NoticeMeetingType?,
+    var participantCapacity: Int? = null,
+
+    var writer: Writer,
+    // 교육과정 대상
+    var targetCourses: List<TargetCourse> = mutableListOf(),
+    // 특강 세션
+    var session: Session,
+){
+    data class Writer(
+        val userId: Long,
+        val userName: String,
+        val profileUrl: String,
+        val role: RoleType
+    )
+
+    data class TargetCourse(
+        val courseId: Long,
+        val courseName: String
+    )
+
+    data class Session(
+        val sessionId: Long,
+        val sessionStartDateTime: LocalDateTime,
+        val sessionEndDateTime: LocalDateTime,
+        val ordinal: Int,
+    )
+}
--- src/main/kotlin/io/sprout/api/notice/repository/NoticeRepositoryCustom.kt
+++ src/main/kotlin/io/sprout/api/notice/repository/NoticeRepositoryCustom.kt
@@ -1,13 +1,13 @@
 package io.sprout.api.notice.repository
 
-import io.sprout.api.notice.model.dto.NoticeCardDto
-import io.sprout.api.notice.model.dto.NoticeDetailResponseDto
-import io.sprout.api.notice.model.dto.NoticeSearchRequestDto
-import io.sprout.api.notice.model.dto.NoticeSearchDto
+import io.sprout.api.notice.model.dto.*
+import io.sprout.api.notice.model.entities.NoticeStatus
+import org.springframework.data.domain.PageRequest
 
 interface NoticeRepositoryCustom {
 
     fun findByIdWithSession(noticeId: Long, userId: Long): List<NoticeDetailResponseDto.Session>
     fun search(searchRequest: NoticeSearchRequestDto, userId: Long): MutableList<NoticeSearchDto>
     fun getApplicationCloseNotice(userId: Long, size: Long, days: Long): MutableList<NoticeCardDto>?
+    fun getSessions(userId: Long, pageable: PageRequest, applicationStatus: NoticeStatus?): MutableList<NoticeSessionResponseDto>?
 }
